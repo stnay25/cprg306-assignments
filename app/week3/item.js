e560efb32bb3123e075487a326792ae894ba560a
stnay25 <123602106+stnay25@users.noreply.github.com>
2023-09-27 14:29:26 -0600

    updated

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,7 +2,7 @@ let list
 
 export default function Item({ name, quantity, category }) {
     return (
-        <div style={{ padding: "1rem", backgroundColor: "cyan", border:"1px solid red", margin:"0.2rem 0"}}>
+        <div style={{ padding: "1rem", backgroundColor: "red", border:"1px solid red", margin:"0.2rem 0"}}>
             <p>{name}</p>
             <p>Buy {quantity} in {category}</p>
         </div>
